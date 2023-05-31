1a26403cb3bb53de4873148c4031affa03f62767
Matteo Buompastore <matteo.buompastore088@gmail.com>
2023-05-31 15:50:40 +0200

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by Matteo Buompastore on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
